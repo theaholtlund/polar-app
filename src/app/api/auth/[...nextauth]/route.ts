0d9ea71135ea4bdb581db84d9645e3715286a9eb
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-03-09 13:33:27 +0100

    Add error handling for missing client ID or client secret in env variables

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,14 @@
-// Set up authorisation for app
+// Set up authorisation for app using route handler
 
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+// Check if environment variables are defined, throw error if not
+if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET) {
+  throw new Error(
+    "GitHub client ID or client secret is not correctly defined in project environment."
+  );
+}
 
 export const authOptions = {
   // Configure one or more authentication providers
