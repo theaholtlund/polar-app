47482ed6155da8a05f26f2a789cfb7cc48db6c84
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-03-20 22:05:21 +0100

    Refine route for app authorisation

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,6 @@
 // Set up authorisation for app using route handler
 
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
-import GithubProvider from "next-auth/providers/github";
-
-// Define default headers
-const defaultHeaders = () => {
-  return {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  };
-};
-
-const headers = (accessToken: string) => {
-  return {
-    ...defaultHeaders(),
-    Authorization: `Bearer ${accessToken}`,
-  };
-};
-
-// Check if environment variables are defined, throw error if not
-if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET) {
-  throw new Error(
-    "GitHub client ID or client secret is not correctly defined in project environment."
-  );
-}
+import NextAuth, { Providers } from "next-auth";
 
 export default NextAuth({
   providers: [
@@ -33,23 +9,25 @@ export default NextAuth({
       name: "Polar",
       type: "oauth",
       version: "2.0",
-      scope: "", // Define the scope according to Polar's API requirements
+      scope: "accesslink.read_all",
       params: { grant_type: "authorization_code" },
       accessTokenUrl: "https://polarremote.com/v2/oauth2/token",
       requestTokenUrl: "https://polarremote.com/v2/oauth2/token",
       authorizationUrl:
-        "https://flow.polar.com/oauth2/authorization?response_type=code",
+        "https://flow.polar.com/oauth2/authorization?response_type=code&scope=accesslink.read_all",
       profileUrl: "https://www.polaraccesslink.com/v3/users",
       clientId: process.env.POLAR_CLIENT_ID,
       clientSecret: process.env.POLAR_CLIENT_SECRET,
-      profile: (profile) => {
+      // TODO:Define a more specific type for the profile object
+      profile: (profile: any) => {
         return {
           id: profile.userId,
           name: profile.firstName + " " + profile.lastName,
-          email: null, // Assume Polar's API might not provide email
+          email: null,
         };
       },
     }),
   ],
+
   // Additional NextAuth configuration as needed
 });
