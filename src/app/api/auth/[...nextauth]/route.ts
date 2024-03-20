32a05d54413389e417c52c4708768c6fff5ff1bb
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-03-20 22:32:24 +0100

    Remove redundant code from authorisation file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,49 @@
-// Set up authorisation for app using route handler
+import NextAuth, { NextAuthOptions } from "next-auth";
 
-import NextAuth, { Providers } from "next-auth";
+// Custom helper function to set HTTP headers for Polar API requests
+const createAuthHeaders = (token: string) => ({
+  Authorization: `Bearer ${token}`,
+  Accept: "application/json",
+  "Content-Type": "application/json",
+});
 
-export default NextAuth({
+// Function to register or update the user in your app's system using Polar's data
+async function updateUserProfile(accessToken: string, userId: string) {
+  const payload = { "member-id": userId };
+
+  await fetch("https://www.polaraccesslink.com/v3/users", {
+    method: "POST",
+    headers: createAuthHeaders(accessToken),
+    body: JSON.stringify(payload),
+  });
+}
+
+// Define NextAuth options
+export const authOptions: NextAuthOptions = {
+  debug: process.env.NODE_ENV === "development",
   providers: [
-    Providers.OAuth({
+    {
       id: "polar",
       name: "Polar",
       type: "oauth",
       version: "2.0",
-      scope: "accesslink.read_all",
-      params: { grant_type: "authorization_code" },
       accessTokenUrl: "https://polarremote.com/v2/oauth2/token",
-      requestTokenUrl: "https://polarremote.com/v2/oauth2/token",
-      authorizationUrl:
-        "https://flow.polar.com/oauth2/authorization?response_type=code&scope=accesslink.read_all",
-      profileUrl: "https://www.polaraccesslink.com/v3/users",
+      authorization: {
+        url: "https://flow.polar.com/oauth2/authorization?response_type=code",
+        params: { scope: "accesslink.read_all" }, // Correctly specifying scope here
+      },
       clientId: process.env.POLAR_CLIENT_ID,
       clientSecret: process.env.POLAR_CLIENT_SECRET,
-      // TODO:Define a more specific type for the profile object
-      profile: (profile: any) => {
+      profile(profile: any) {
         return {
-          id: profile.userId,
-          name: profile.firstName + " " + profile.lastName,
-          email: null,
+          id: profile["polar-user-id"],
+          name: `${profile["first-name"]} ${profile["last-name"]}`,
+          email: profile.email, // Assuming Polar provides an email, otherwise set to null
         };
       },
-    }),
+    },
   ],
-
   // Additional NextAuth configuration as needed
-});
+};
+
+export default NextAuth(authOptions);
