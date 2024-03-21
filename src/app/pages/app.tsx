7d43af568e73ac2d929d90855b6db4f18211dd52
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-03-21 13:44:06 +0100

    Add file for specifying custom paths

---
+++ src/app/pages/app.tsx
@@ -0,0 +1,15 @@
+// Import types and functionality from types and functionality
+import { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import "../styles/globals.css";
+
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
