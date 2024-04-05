4090f88dea7ea256d58054be40dcbfcb58bfd9f3
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-04-05 21:34:20 +0200

    Update next configs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
-// Import types and functionality from types and functionality
 /** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-// Make  env variables available to application
-const nextConfig = {
-  env: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    POLAR_CLIENT_ID: process.env.POLAR_CLIENT_ID,
-    POLAR_CLIENT_SECRET: process.env.POLAR_CLIENT_SECRET,
-    BASEURL: process.env.BASEURL,
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
