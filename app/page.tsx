47235655e6c3e26f66af51e61e16f7818f82ddc5
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-04-05 21:33:23 +0200

    Reformat page and layout structure

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+// Define home page component for the app
+export default function App() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-2xl font-semibold tracking-tight">Home page</h1>
+      </div>
+    </section>
+  );
+}
