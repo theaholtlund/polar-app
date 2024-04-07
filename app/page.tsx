f9c14210eebe9a3f1ab521c36d69da8dbb241d55
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-04-07 09:10:57 +0200

    Move logo below home page title

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function App() {
   return (
     <section className="py-24 text-center">
       <div className="container mx-auto">
-        <img src="/logo-2.webp" alt="Logo" className="mx-auto w-32 mb-4" />{" "}
         <h1 className="text-2xl font-semibold tracking-tight">Home page</h1>
+        <img src="/logo-2.webp" alt="Logo" className="mx-auto w-32 mt-4" />
       </div>
     </section>
   );
