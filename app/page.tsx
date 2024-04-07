dcb4cdd140f154568629466bbce240b51e421043
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-04-07 09:09:50 +0200

    Add logo to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 // Define home page component for the app
 export default function App() {
   return (
-    <section className="py-24">
-      <div className="container">
+    <section className="py-24 text-center">
+      <div className="container mx-auto">
+        <img src="/logo-2.webp" alt="Logo" className="mx-auto w-32 mb-4" />{" "}
         <h1 className="text-2xl font-semibold tracking-tight">Home page</h1>
       </div>
     </section>
