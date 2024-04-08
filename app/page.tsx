10af91e8f8e178bd56fe02e5b8c8c05a6e35ee59
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-04-08 10:21:07 +0200

    Update content on home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,13 @@ export default function App() {
   return (
     <section className="py-24 text-center">
       <div className="container mx-auto">
-        <h1 className="text-2xl font-semibold tracking-tight">Home page</h1>
+        <h1 className="text-2xl font-semibold tracking-tight">
+          Polar Data Analysis
+        </h1>
         <img src="/logo-2.webp" alt="Logo" className="mx-auto w-32 mt-4" />
+        <p className="mt-20 text-lg font-medium">
+          Sign in to get started analysing your Polar training data :)
+        </p>
       </div>
     </section>
   );
