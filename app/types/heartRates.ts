2fb7ad5400c23b7965572a5922a0572d2d6c0b76
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-05-10 10:14:52 +0200

    Update comments for types

--- app/types/heartRates.ts
+++ app/types/heartRates.ts
@@ -1,10 +1,11 @@
-// Type definition for the props expected
+// Type definition for the props expected from HeartRateData
 export interface HeartRateData {
   polar_user: string;
   date: string;
   heart_rate_samples: HeartRateSamples[];
 }
 
+// Type definition for the props expected from HeartRateSamples
 export interface HeartRateSamples {
   heart_rate: number;
   sample_time: string;
